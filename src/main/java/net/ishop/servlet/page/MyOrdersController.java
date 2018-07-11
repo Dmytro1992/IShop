e2b725a412c7b57b288f89f23b2b6da75f6954f7
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 22:40:58 +0300

    Add,Remove product, Show shopping cart, sync shopping cart

---
+++ src/main/java/net/ishop/servlet/page/MyOrdersController.java
@@ -0,0 +1,32 @@
+package net.ishop.servlet.page;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.entity.Order;
+import net.ishop.model.CurrentAccount;
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+import net.ishop.util.SessionUtils;
+
+
+@WebServlet("/my-orders")
+public class MyOrdersController extends AbstractController {
+	private static final long serialVersionUID = -1782066337808445826L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		CurrentAccount currentAccount = SessionUtils.getCurrentAccount(req);
+		List<Order> orders = getOrderService().listMyOrders(currentAccount, 1, Constants.ORDERS_PER_PAGE);
+		req.setAttribute("orders", orders);
+		int orderCount = getOrderService().countMyOrders(currentAccount);
+		req.setAttribute("pageCount", getPageCount(orderCount, Constants.ORDERS_PER_PAGE));
+		RoutingUtils.forwardToPage("my-orders.jsp", req, resp);
+	}
+}
---
+++ src/main/java/net/ishop/servlet/page/ShowShoppingCartController.java
@@ -0,0 +1,27 @@
+package net.ishop.servlet.page;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+import net.ishop.util.SessionUtils;
+
+
+@WebServlet("/shopping-cart")
+public class ShowShoppingCartController extends AbstractController {
+	private static final long serialVersionUID = -1916373553298888514L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (SessionUtils.isCurrentShoppingCartCreated(req)) {
+			RoutingUtils.forwardToPage("shopping-cart.jsp", req, resp);
+		} else {
+			RoutingUtils.redirect("/products", req, resp);
+		}
+	}
+}
