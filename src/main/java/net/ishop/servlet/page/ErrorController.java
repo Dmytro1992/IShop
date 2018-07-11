22dac8649c36ed4fc2dc599f098e116806d75407
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 22:39:48 +0300

    Error processing

---
+++ src/main/java/net/ishop/servlet/page/ErrorController.java
@@ -0,0 +1,23 @@
+package net.ishop.servlet.page;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+
+
+@WebServlet("/error")
+public class ErrorController extends AbstractController {
+	private static final long serialVersionUID = -4385792519039493271L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("statusCode", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		RoutingUtils.forwardToPage("error.jsp", req, resp);
+	}
+}
