4b6fe64b3227e6dc3b597901276234280190f554
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 22:39:15 +0300

    Pagination and Search

---
+++ src/main/java/net/ishop/servlet/page/SearchController.java
@@ -0,0 +1,33 @@
+package net.ishop.servlet.page;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.entity.Product;
+import net.ishop.form.SearchForm;
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+
+
+@WebServlet("/search")
+public class SearchController extends AbstractController {
+	private static final long serialVersionUID = 1015660808630879774L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		SearchForm searchForm = createSearchForm(req);
+		List<Product> products = getProductService().listProductsBySearchForm(searchForm, 1, Constants.MAX_PRODUCTS_PER_HTML_PAGE);
+		req.setAttribute("products", products);
+		int totalCount = getProductService().countProductsBySearchForm(searchForm);
+		req.setAttribute("pageCount", getPageCount(totalCount, Constants.MAX_PRODUCTS_PER_HTML_PAGE));
+		req.setAttribute("productCount", totalCount);
+		req.setAttribute("searchForm", searchForm);
+		RoutingUtils.forwardToPage("search-result.jsp", req, resp);
+	}
+}
