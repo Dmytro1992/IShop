26f57ad3e85ebce1f71d8192cf58d7bdb113ddcd
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 17:42:03 +0300

    Final review

---
+++ src/main/java/net/ishop/filter/CheckAuthentificationFilter.java
@@ -0,0 +1,36 @@
+package net.ishop.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.util.RoutingUtils;
+import net.ishop.util.SessionUtils;
+import net.ishop.util.UrlUtils;
+import net.ishop.util.WebUtils;
+
+
+@WebFilter(filterName = "CheckAuthentificationFilter")
+public class CheckAuthentificationFilter extends AbstractFilter {
+
+	@Override
+	public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		if (SessionUtils.isCurrentAccountCreated(req)) {
+			chain.doFilter(req, resp);
+		} else {
+			String requestUrl = WebUtils.getCurrentRequestUrl(req);
+			if (UrlUtils.isAjaxUrl(requestUrl)) {
+				resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				RoutingUtils.sendHTMLFragment("401", req, resp);
+			} else {
+				req.getSession().setAttribute(Constants.SUCCESS_REDIRECT_URL_AFTER_SIGNIN, requestUrl);
+				RoutingUtils.redirect("/sign-in", req, resp);
+			}
+		}
+	}
+}
