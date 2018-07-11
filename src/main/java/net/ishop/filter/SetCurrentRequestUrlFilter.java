e9808d4c4de0d911d8547e57d4a799ab5e7ac955
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 17:42:30 +0300

    Make order + Signin/out

---
+++ src/main/java/net/ishop/filter/SetCurrentRequestUrlFilter.java
@@ -0,0 +1,22 @@
+package net.ishop.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.util.WebUtils;
+
+
+@WebFilter(filterName="SetCurrentRequestUrlFilter")
+public class SetCurrentRequestUrlFilter extends AbstractFilter {
+	@Override
+	public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		req.setAttribute(Constants.CURRENT_REQUEST_URL, WebUtils.getCurrentRequestUrl(req));
+		chain.doFilter(req, resp);
+	}
+}
