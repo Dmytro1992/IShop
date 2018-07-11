40f565de76ff8f5ce0f9c8b164408118ef5fddac
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 22:55:10 +0300

    login facebook

---
+++ src/main/java/net/ishop/model/SocialAccount.java
@@ -0,0 +1,21 @@
+package net.ishop.model;
+
+
+public class SocialAccount {
+	private final String name;
+	private final String email;
+
+	public SocialAccount(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+}
