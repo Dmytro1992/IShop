e962a97fcae3a831235a2af83e853f0defb081c4
Dmytro1992 <dishumov@yandex.ru>
2018-07-11 22:24:55 +0300

    work ajax query

---
+++ src/main/java/net/ishop/servlet/ajax/ProductsByCategoryMoreController.java
@@ -0,0 +1,29 @@
+package net.ishop.servlet.ajax;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.entity.Product;
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+
+
+@WebServlet("/ajax/html/more/products/*")
+public class ProductsByCategoryMoreController extends AbstractController {
+	private static final long serialVersionUID = -2651974520717714088L;
+	private static final int SUBSTRING_INDEX = "/ajax/html/more/products".length();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String categoryUrl = req.getRequestURI().substring(SUBSTRING_INDEX);
+		List<Product> products = getProductService().listProductsByCategory(categoryUrl, getPage(req), Constants.MAX_PRODUCTS_PER_HTML_PAGE);
+		req.setAttribute("products", products);
+		RoutingUtils.forwardToFragment("product-list.jsp", req, resp);
+	}
+}
---
+++ src/main/java/net/ishop/servlet/ajax/SearchResultsMoreController.java
@@ -0,0 +1,29 @@
+package net.ishop.servlet.ajax;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ishop.Constants;
+import net.ishop.entity.Product;
+import net.ishop.form.SearchForm;
+import net.ishop.servlet.AbstractController;
+import net.ishop.util.RoutingUtils;
+
+
+@WebServlet("/ajax/html/more/search")
+public class SearchResultsMoreController extends AbstractController {
+	private static final long serialVersionUID = -2651974520717714088L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		SearchForm searchForm = createSearchForm(req);
+		List<Product> products = getProductService().listProductsBySearchForm(searchForm, getPage(req), Constants.MAX_PRODUCTS_PER_HTML_PAGE);
+		req.setAttribute("products", products);
+		RoutingUtils.forwardToFragment("product-list.jsp", req, resp);
+	}
+}
